c15156870b32487a35cd50ce305caf051497429f
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-02-25 21:25:05 +0100

    first commit

--- members/views.py
+++ members/views.py
@@ -1,9 +1,3 @@
-# from django.shortcuts import render
-# from django.http import HttpResponse
-
-# def members(request):
-#     return HttpResponse("Hello world!")
-
 from django.http import HttpResponse
 from django.template import loader
 from .models import Member
@@ -13,5 +7,13 @@ def members(request):
   template = loader.get_template('all_members.html')
   context = {
     'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+  
+def details(request, id):
+  mymember = Member.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
   }
   return HttpResponse(template.render(context, request))
